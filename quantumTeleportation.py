083f0a976f3ce251f6054b6b7bef5b2bac76fb54
dr3du4 <jan.krzyszkowski2@gmail.com>
2024-04-12 23:45:06 +0200

    finish qubits teleportation

--- quantumTeleportation.py
+++ quantumTeleportation.py
@@ -1,19 +1,18 @@
 from qiskit import *
-
+from qiskit_aer import *
 # Quantum program setup
 
 
 
-Q_program = QuantumProgram()
 
 # Creating registers
-q = Q_program.create_quantum_register('q', 3)
-c0 = Q_program.create_classical_register('c0', 1)
-c1 = Q_program.create_classical_register('c1', 1)
-c2 = Q_program.create_classical_register('c2', 1)
+q = QuantumRegister(3, 'q')
+c0 = ClassicalRegister(1, 'c0')
+c1 = ClassicalRegister(1, 'c1')
+c2 = ClassicalRegister(1, 'c2')
 
 # Creates the quantum circuit
-teleport = Q_program.create_circuit('teleport', [q], [c0,c1,c2])
+teleport = QuantumCircuit(q, c0,c1,c2)
 
 # Make the shared entangled state
 teleport.h(q[1])
@@ -37,20 +36,15 @@ teleport.x(q[2]).c_if(c1, 1)
 # Bob checks the state of the teleported qubit
 teleport.measure(q[2], c2[0])
 
-# Shows gates of the circuit
-circuits = ['teleport']
-print(Q_program.get_qasms(circuits)[0])
+simulator = Aer.get_backend('qasm_simulator')
 
-# Parameters for execution on simulator
-backend = 'local_qasm_simulator'
-shots = 1024 # the number of shots in the experiment
+# Transpile and assemble the circuit
+transpiled_circuit = transpile(teleport, simulator)
+qobj = assemble(transpiled_circuit, shots=1024)
 
 # Run the algorithm
-result = Q_program.execute(circuits, backend=backend, shots=shots)
+result = simulator.run(qobj).result()
 
 #Shows the results obtained from the quantum algorithm
-counts = result.get_counts('teleport')
-
+counts = result.get_counts(teleport)
 print('\nThe measured outcomes of the circuits are:', counts)
-
-# credits to:  https://github.com/QISKit/qiskit-tutorial
